7c301e3c4f47fbadcdbafb2b0cbfac642595cd38
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-08-27 00:24:23 -0300

    Create Pagamento.cs

---
+++ comercioBackendv1/Pagamentos/3-Dominio/Pagamentos.Dominio/Entidades/Pagamento.cs
@@ -0,0 +1,15 @@
+﻿namespace Pagamentos.Dominio.Entidades
+{
+    //[Table("tb_pagamento")]
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Dt_entrega{ get; set; }
+        public DateTime Dt_vencimento { get; set; }
+        public int Num_parcela { get; set; }
+        public int Qtd_parcela { get; set; }
+        public int NotaFiscal { get; set; }
+        public int Fornecedor_id { get; set; }
+    }
+}
