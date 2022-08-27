045f3e3c5a01993a402ea89b3fd16e84ffc1b608
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-08-27 00:24:15 -0300

    Create Initializer.cs

---
+++ comercioBackendv1/Pagamentos/2-Aplicacao/Pagamentos.Aplicacao/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pagamentos.Aplicacao.Adapter;
+using Pagamentos.Aplicacao.Adapter.Interfaces;
+using Pagamentos.Aplicacao.Handles;
+using Pagamentos.Aplicacao.Handles.Interfaces;
+
+namespace Pagamentos.Aplicacao
+{
+    public static class Initializer
+    {
+        public static void ConfigureIocDI(this IServiceCollection services)
+        {
+            services.AddScoped<IInserirPagamentoHandle, InserirPagamentoHandle>();
+            services.AddScoped<IObterPagamentoHandle, ObterPagamentoHandle>();
+            services.AddScoped<IAtualizarPagamentoHandle, AtualizarPagamentoHandle>();
+            services.AddScoped<IExcluirPagamentoHandle, ExcluirPagamentoHandle>();
+            services.AddScoped<IPagamentosAdapter, PagamentosAdapter>();
+        }
+    }
+}
